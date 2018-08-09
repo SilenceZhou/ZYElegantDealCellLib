3e656e7da4da906298ba64407e4d1eb3839aba71
SilenceZhou <1063123603@qq.com>
2018-08-09 12:07:45 +0800

    Initial commit

---
+++ Example/ZYElegantDealCellLib/ZYViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYViewController.h
+//  ZYElegantDealCellLib
+//
+//  Created by SilenceZhou on 08/09/2018.
+//  Copyright (c) 2018 SilenceZhou. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZYViewController : UIViewController
+
+@end
---
+++ Example/ZYElegantDealCellLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZYElegantDealCellLib
+//
+//  Created by SilenceZhou on 08/09/2018.
+//  Copyright (c) 2018 SilenceZhou. All rights reserved.
+//
+
+@import UIKit;
+#import "ZYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYAppDelegate class]));
+    }
+}
